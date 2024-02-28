bc3539788731b4cda9f07ae4c1dc2d45690b27db
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-02-28 21:43:25 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-всем привет
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <title>Sugar Shack</title>
+</head>
+<body>
+
+    <div class="header">
+        <div class="header-left">
+            Главная
+            Новости
+            О нас
+            Обратная связь
+        </div>
+        <div class="header-right">
+            Авторизация
+            Регистрация
+        </div>
+    </div>
+
+    <div class="slider">
+    <div id="carouselExample" class="carousel slide w-100 h-80">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="img/1.jpg" class="d-block w-90 h-100 d-flex m-auto rounded">
+            </div>
+            <div class="carousel-item">
+            <img src="img/2.jpg" class="d-block w-80 h-100 d-flex m-auto rounded">
+            </div>
+            <div class="carousel-item">
+            <img src="img/3.jpg" class="d-block w-80 h-100 d-flex m-auto rounded">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <i class="bi bi-arrow-left-circle-fill" style="color: #EC7088"></i>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <i class="bi bi-arrow-right-circle-fill" style="color: #EC7088"></i>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    </div>
+
+</body>
+</html>
