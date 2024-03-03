a163887ab9cd5c10a3ea1f4ca5bdba6d2fe4f897
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-03 05:29:46 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-document.querySelectorAll('.news-block').forEach(newsBlock => {
-    newsBlock.addEventListener('click', function() {
-        const newsId = this.dataset.id;
-        window.location.href = `open_news.php?id=${newsId}`;
+var newsBlocks = document.querySelectorAll('.news-block');
+
+// Проходимся по каждому блоку и добавляем обработчик события щелчка
+newsBlocks.forEach(function(block) {
+    block.addEventListener('click', function() {
+        // Получаем идентификатор новости из атрибута data-id
+        var newsId = block.getAttribute('data-id');
+
+        // Перенаправляем пользователя на страницу новости с помощью window.location
+        window.location.href = 'open_news.php?id=' + newsId;
     });
 });
