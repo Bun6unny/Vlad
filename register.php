dbd6e8c741ddc9e9af680475c44d9179f8e76249
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-04 00:12:18 +0500

    123

--- register.php
+++ register.php
@@ -1,29 +1,29 @@
 <?php
-    $host = 'localhost';
-    $username = '2is-b11_2is-b11';
-    $password = 'dTav2z8hny';
-    $database = '2is-b11_2is-b11';
+$host = 'localhost';
+$username = '2is-b11_2is-b11';
+$password = 'dTav2z8hny';
+$database = '2is-b11_2is-b11';
 
-    $mysqli = new mysqli($host, $username, $password, $database);
+$mysqli = new mysqli($host, $username, $password, $database);
 
-    if ($mysqli->connect_errno) {
-        echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-        exit();
-    }
+if ($mysqli->connect_errno) {
+    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    exit();
+}
 
-    $login = $_POST['login'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+$login = $_POST['login'];
 
-    $stmt = $mysqli->prepare("INSERT INTO Users (Login, Password, Mail) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $login, $password, $email);
+$sql = "SELECT * FROM Users WHERE Login = ?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $login);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    echo "exists"; // Логин уже занят
+} else {
+    echo "available"; // Логин свободен
+}
 
-    if ($stmt->execute()) {
-        echo "Пользователь успешно зарегистрирован";
-    } else {
-        echo "Ошибка: " . $stmt->error;
-    }
-
-    $stmt->close();
-    $mysqli->close();
+$stmt->close();
+$mysqli->close();
 ?>
