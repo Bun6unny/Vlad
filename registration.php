33dc7e09b210a463c01b17a9d9ce52f9d6b2731b
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-03 23:49:48 +0500

    hand

--- registration.php
+++ registration.php
@@ -66,51 +66,8 @@
     <div class="top-name" style="width:97%;">
         Регистрация
     </div>
-
-    <?php
-    // Подключение к базе данных
-    $host = 'localhost';
-    $username = '2is-b11_2is-b11';
-    $password = 'dTav2z8hny';
-    $database = '2is-b11_2is-b11';
-
-    $mysqli = new mysqli($host, $username, $password, $database);
-
-    if ($mysqli->connect_errno) {
-        echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
-
-    // Проверка, была ли отправлена форма
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Получение данных из формы
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        
-        // Защита от SQL-инъекций
-        $name = $mysqli->real_escape_string($name);
-        $email = $mysqli->real_escape_string($email);
-        $password = $mysqli->real_escape_string($password);
-        
-        // Хеширование пароля
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        
-        // Вставка данных в таблицу Users
-        $sql = "INSERT INTO Users (login, mail, password) VALUES ('$name', '$email', '$hashed_password')";
-        
-        if ($mysqli->query($sql)) {
-            // Редирект на страницу login.php
-            header("Location: login.php");
-            exit();
-        } else {
-            echo "Ошибка: " . $mysqli->error;
-        }
-    }
-
-    // Закрытие соединения с базой данных
-    $mysqli->close();
-    ?>
     
+    <form method="post" action="register_handler.php">
     <div class="reg">
         <div class="reg-left">Введите ваше имя (Логин):</div>
         <div class="reg-right">
@@ -138,6 +95,7 @@
     <div class="reg">
         <button class="reg-button" type="submit">Зарегистрироваться</button>
     </div>
+    </form>
     <div class="reg">
         <div class="already">Уже есть аккаунт? Авторизируйтесь:</div>
         <div class="to-login">
