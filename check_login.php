0501af10811a5efc10aeaa0bc2de1f0a6ea76789
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-04 00:16:45 +0500

    123

---
+++ check_login.php
@@ -0,0 +1,29 @@
+<?php
+$host = 'localhost';
+$username = '2is-b11_2is-b11';
+$password = 'dTav2z8hny';
+$database = '2is-b11_2is-b11';
+
+$mysqli = new mysqli($host, $username, $password, $database);
+
+if ($mysqli->connect_errno) {
+    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    exit();
+}
+
+$login = $_POST['login'];
+
+$sql = "SELECT * FROM Users WHERE Login = ?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $login);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    echo "exists"; // Логин уже занят
+} else {
+    echo "available"; // Логин свободен
+}
+
+$stmt->close();
+$mysqli->close();
+?>
