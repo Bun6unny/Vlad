3b82818d0c7496f745f170c265dea374a90959f7
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-16 05:57:53 +0500

    1

--- logout.php
+++ logout.php
@@ -1,8 +1,6 @@
 <?php
     session_start();
-    // Завершаем сессию
     session_destroy();
-    // Перенаправляем пользователя на страницу входа или другую страницу
     header("Location: index.php");
     exit;
 ?>
