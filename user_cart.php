2b373264c4965020279bec008f94ce92be801afe
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-21 22:53:56 +0500

    1

--- user_cart.php
+++ user_cart.php
@@ -169,7 +169,7 @@
                                     <div class="item-cart">';
                                 echo "<div class='item-image'><img src='{$row_candy['Image']}' alt='{$item}' style='max-width:90%;max-height:90%;border-radius:0.3vw;'></div>
                                     <div class='item-right'>
-                                    <div class='item-price' style='font-size:1.8vw;font-weight:bold;'>   
+                                    <div class='item-price' style='font-size:1.8vw;font-weight:bold;margin-top:5%;'>   
                                     {$row_candy['Name']}
                                     </div>
                                     <div class='item-price' style='font-size:1.5vw;'>  
