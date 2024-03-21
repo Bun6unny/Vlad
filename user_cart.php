6dba549761c4c13e42dc9d35c0fba0174c3212bc
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-21 23:39:14 +0500

    1

--- user_cart.php
+++ user_cart.php
@@ -287,7 +287,7 @@
                 echo '<div class="item-order">';
                 echo '<div class="item-order-box">';
                 echo '<div style="width:100%;height:25%;line-height:1.2;font-size:1.8vw;text-align:center;display: flex;align-items: center;justify-content: center;">Оформление товара</div>';
-                echo "<div style='width:100%;height:15%;line-height:1.2;font-size:1.5vw;text-align:center;display: flex;align-items: center;justify-content: center;'>Общая стоимость: $totalPrice ₽</div>";
+                echo "<div style='width:100%;height:15%;line-height:1.2;font-size:1.2vw;text-align:center;display: flex;align-items: center;justify-content: center;'>Общая стоимость: $totalPrice ₽</div>";
                 echo "Всего товаров на странице: $totalItems";
                 echo '<form method="post" action="user_cart.php">
                         <button type="submit" name="clear_items">Очистить столбец items</button>
