9ae1de396b4d33c5a64be781d39a2b77578fa206
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-26 01:34:16 +0500

    fb_upd

---
+++ submit.php
@@ -0,0 +1,29 @@
+<?php
+    $host = 'localhost';
+    $username = '2is-b11_2is-b11';
+    $password = 'dTav2z8hny';
+    $database = '2is-b11_2is-b11';
+
+    $mysqli = new mysqli($host, $username, $password, $database);
+
+    if ($mysqli->connect_errno) {
+        echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+
+    if (isset($_POST['submit'])) {
+        $mail = $_POST['mail'];
+        $message = $_POST['message'];
+
+        $mail = $mysqli->real_escape_string($mail);
+        $message = $mysqli->real_escape_string($message);
+
+        $sql = "INSERT INTO Messages (Mail, Message) VALUES ('$mail', '$message')";
+        if ($mysqli->query($sql) === TRUE) {
+            echo "Сообщение успешно отправлено!";
+        } else {
+            echo "Ошибка: " . $sql . "<br>" . $mysqli->error;
+        }
+
+        $mysqli->close();
+    }
+?>
