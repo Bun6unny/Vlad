564b3158db3fb6796fca4f7ee6af8af915e9cd74
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-31 07:21:09 +0500

    Update user.php

--- user.php
+++ user.php
@@ -170,7 +170,8 @@
                         <div class="user-line"><div class="user-line-adaptive" style="display:flex;align-items:center;justify-content:flex-end;">Сменить аватар:</div><input id="new-avatar" type="text" placeholder="..." class="user-input">
                             <button id="change-avatar-btn" class="change-button">Изменить аватар</button>
                         </div>
-                        <div class="user-line user-down-line">Только URL-ссылки!</div>
+                        <div class="user-line phone-div" style="height:7%;width:48.5%;justify-content:flex-end;font-size:1vw;">Только URL-ссылки!</div>
+                        <div class="user-line computer-div" style="height:7%;width:48.5%;justify-content:flex-end;font-size:1vw;">Только URL-ссылки!</div>
                         <button class="change-button" style="margin-left:82%;width:15%;height:10%;" id="delete-account">Удалить аккаунт</button>
                     </div>
                 </div>';
