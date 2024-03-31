02b35100bd441a606d49c6aaa9dc60cb32358e00
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-31 06:34:43 +0500

    Update catalog.php

--- catalog.php
+++ catalog.php
@@ -263,6 +263,11 @@
         $mysqli->close();
     ?>
 
+    <div class="computer-div"></div>
+    <div class="phone-div">
+        <div style="height:50%"></div>
+    </div>
+
     <div class="footer" style="margin-top:1%;">
         <div class="footer-left">
         ФИО: Ходырев Владислав Вадимович<br>
