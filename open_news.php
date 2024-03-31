f4c59493590c1b2a46a992d0afa377b21f4393fe
Bun6unny <124562705+Bun6unny@users.noreply.github.com>
2024-03-31 05:11:51 +0500

    Update open_news.php

--- open_news.php
+++ open_news.php
@@ -105,8 +105,8 @@
         $row = $result->fetch_assoc();
         echo "<div class='top-name'>{$row['Head']}</div>";
         echo "<div class='open-news'>";
-        echo "<div class='computer-div' style='width:21%;height:80%;display: flex;align-items: center;justify-content: center;'><img src='{$row['Image']}' alt='Изображение новости' style='max-width:95%;max-height:95%;border-radius:0.3vw;border:0.1vw solid #EC7088;'></div>";
-        echo "<div class='phone-div' style='width:35%;height:80%;display: flex;align-items: center;justify-content: center;'><img src='{$row['Image']}' alt='Изображение новости' style='max-width:95%;max-height:95%;border-radius:0.3vw;border:0.1vw solid #EC7088;'></div>";
+        echo "<div class='computer-div' style='width:21%;height:80%;display: flex;align-items: center;justify-content: center;'><img class='computer-img' src='{$row['Image']}' alt='Изображение новости' style='max-width:95%;max-height:95%;border-radius:0.3vw;border:0.1vw solid #EC7088;'></div>";
+        echo "<div class='phone-div' style='width:35%;height:80%;display: flex;align-items: center;justify-content: center;'><img class='phone-img' src='{$row['Image']}' alt='Изображение новости' style='max-width:95%;max-height:95%;border-radius:0.3vw;border:0.1vw solid #EC7088;'></div>";
         echo "<div class='open-news-text'>{$row['full']}</div>";
         echo "</div>";
         echo "<div style='width:100%;height:15%;display:flex;align-items: center;justify-content: center;'>";
